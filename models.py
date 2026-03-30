ba682924c33e05c1e7914692dda211789c85cc3c
Chandramani Chaudhary <chandramanichaudhary1116@gmail.com>
2026-03-30 09:03:58 +0545

    key updataed

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os
 from google import genai
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key="AIzaSyB2-x303ZdbmYGtXtgeIFtH8vcim3AC1PU")
 def gemini_llm(text):
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
